bdc794c60c02e9b888e4c5ac2c6b5d620ca5186d
kennethtsui <kennethtsui15@gmail.com>
2017-03-26 21:17:49 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,19 @@
+// name, atk, hp, cost, img
+Card0=["Royal Giant",5,10,6,""];                // 皇家巨人
+Card1=["Globlin",2,3,2,""];                     // 哥布林
+Card2=["Berserker",7,5,4,""];                  // 狂戰士
+Card3=["Goblin Archer",2,4,2,"攻撃最低HP的敵人"];    // 攻撃最低hp 哥布林弓箭手
+Card4=["Dwarven Archers",3,5,4,"攻撃最高HP的敵人"];  // 攻撃最高攻 矮人弓箭手
+Card5=["Elven Archer",3,5,4,"攻撃最高攻擊力的敵人"];  // 攻撃最高攻 精靈弓箭手
+Card6=["Bowler",3,5,7,"Attack All"];               // 投石手
+Card7=["Healer",0,5,4,"hp + 1"];                   // 治療師
+Card8=["Wizard",5,5,4,""];                         // 男巫
+Card9=["Witch",2,5,4,,"吸取對手2HP"];                // 女巫
+Card10=["Resouce++",0,0,4,,"提升資源吸取量(+2)"];                // 女巫
+Card11=["lightning",2,0,3,,"造成2HP傷害"];                // 
+Card12=["Snowstorm",7,0,4,,"造成7HP傷害"];                // 暴風雪
+Card13=["Earthquake",10,0,4,,"造成10HP傷害"];                // 地震
+Card14=["HP++",0,0,4,,"提升所有卡牌2HP"];                // 
+Card15=["My HP++",0,0,4,,"提升自己10HP"];                // 
+Card16=["Tsunami",0,0,8,,"掃場"];                // 
+Card17=["Paralysis",0,0,4,,"麻痺所有對手"];          //
